809980c5942f4f8dac44bcaa232c5761d9853d06
cosmic-fi <killrbid@gmail.com>
2025-07-28 18:47:28 +0300

    refactor: update project branding and authentication exports

--- src/Index.ts
+++ src/Index.ts
@@ -3,12 +3,18 @@
  * Luuxis License v1.0 (voir fichier LICENSE pour les détails en FR/EN)
  */
 
+import AZauth from './Authenticator/AZauth.js';
 import Launch from './Launch.js';
+import Microsoft from './Authenticator/Microsoft.js';
+import * as Mojang from './Authenticator/Mojang.js';
 import Status from './StatusServer/status.js';
 import Downloader from './utils/Downloader.js';
 
 export {
+    AZauth as AZauth,
     Launch as Launch,
+    Microsoft as Microsoft,
+    Mojang as Mojang,
     Status as Status,
     Downloader as Downloader
 };
